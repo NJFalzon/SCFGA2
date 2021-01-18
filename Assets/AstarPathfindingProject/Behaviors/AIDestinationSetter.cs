e57b69546c25f106fade054c4de47b5a4f87e834
NJFalzon <nathan.falzon.c10326@mcast.edu.mt>
2021-01-18 17:04:15 +0100

    Set up the level and Designed Level 1

---
+++ Assets/AstarPathfindingProject/Behaviors/AIDestinationSetter.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+namespace Pathfinding {
+	
+	[UniqueComponent(tag = "ai.destination")]
+	[HelpURL("http://arongranberg.com/astar/docs/class_pathfinding_1_1_a_i_destination_setter.php")]
+	public class AIDestinationSetter : VersionedMonoBehaviour {
+		
+		public Transform target;
+		IAstarAI ai;
+
+		void OnEnable () {
+			ai = GetComponent<IAstarAI>();
+			if (ai != null) ai.onSearchPath += Update;
+		}
+
+		void OnDisable () {
+			if (ai != null) ai.onSearchPath -= Update;
+		}
+
+		void Update () {
+			if (target != null && ai != null)
+			{
+				ai.destination = target.position;
+			}
+		}
+	}
+}

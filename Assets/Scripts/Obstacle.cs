9f219d4ef4d9f6e64e62e8a9831db5d211922cd6
NJFalzon <nathan.falzon.c10326@mcast.edu.mt>
2021-01-21 15:12:20 +0100

    Level 3 Design and completed, Obstacles implemented and are avoided by the AI

---
+++ Assets/Scripts/Obstacle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Obstacle : MonoBehaviour
+{
+    [SerializeField] Vector3 posA;
+    [SerializeField] Vector3 posB;
+
+    bool isPosA = true;
+
+    float timer = 0;
+    readonly float maxTimer = 0.5f;
+
+
+    private void Update()
+    {
+        if (isPosA)
+        {
+            Move(posB);
+        }
+        else
+        {
+            Move(posA);
+        }
+    }
+
+    void Move(Vector3 pos)
+    {
+        if((timer-=Time.deltaTime) <= 0)
+        {
+            this.transform.position = Vector3.MoveTowards(this.transform.position, pos, 1f);
+            timer = maxTimer;
+        }
+
+        if(transform.position == pos)
+        {
+            isPosA = !isPosA;
+        }
+    }
+}

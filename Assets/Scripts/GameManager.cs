e300f6811df9fbeaffd35885e861ac4a4e9ac3e0
NJFalzon <nathan.falzon.c10326@mcast.edu.mt>
2021-01-22 12:13:14 +0100

    Start Scene Coded and ready to be used by PlayerPrefs

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager Instance { get; private set; }
+
+    [SerializeField] private string username;
+    [SerializeField] private float time;
+
+    private bool started = false;
+    private bool ended = false;
+
+
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+        }
+        else if(Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        Timer();
+    }
+
+
+    void Timer()
+    {
+        if (started && !ended)
+        {
+            time += Time.deltaTime;
+        }
+    }
+
+
+
+    public void SetUsername(string username)
+    {
+        this.username = username;
+    }
+
+    public string GetUsername()
+    {
+        return username;
+    }
+
+    public void StartTimer()
+    {
+        started = true;
+        SceneManager.LoadScene("Level1");
+    }
+
+    public float GetTime()
+    {
+        return time;
+    }
+}
---
+++ Assets/Scripts/StartGameScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartGameScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void UserName()
+    {
+        string name = GetComponentInChildren<InputField>().text;
+        GameObject.Find("GameManager").GetComponent<GameManager>().SetUsername(name);
+
+        if (name.Length < 3)
+        {
+            GetComponentInChildren<Button>().interactable = false;
+        }
+        else
+        {
+            GetComponentInChildren<Button>().interactable = true;
+        }
+    }
+}
